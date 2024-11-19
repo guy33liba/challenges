25528d009183ee73f01c4e68f15376eb48280b81
guy33liba <guy33liba@gmail.com>
2024-11-19 21:13:49 +0200

    \asas

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import PalindromeChecker from "./challenges/PalindromeChecker"
 import PrimeChecker from "./challenges/PrimeChecker"
 import AnagramChecker from "./challenges/AnagramChecker"
 import Fibonacci from "./challenges/Fibonacci"
+import TodoList from "./challenges/TodoList"
 const App = () => {
-  return (
-    <div className="app">
-      {/* <PalindromeChecker /> */}
-      {/* <PrimeChecker /> */}
-      {/* <AnagramChecker /> */}
-      <Fibonacci />
-    </div>
-  )
+ return (
+  <div className="app">
+   {/* <PalindromeChecker /> */}
+   {/* <PrimeChecker /> */}
+   {/* <AnagramChecker /> */}
+   {<TodoList />}
+   {/* <Fibonacci /> */}
+  </div>
+ )
 }
 
 export default App
