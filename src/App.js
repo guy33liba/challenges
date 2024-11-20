54255e50f348c806b277c88365275d6fbb5efba1
guy33liba <guy33liba@gmail.com>
2024-11-20 08:40:16 +0200

    capitilize

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import PrimeChecker from "./challenges/PrimeChecker"
 import AnagramChecker from "./challenges/AnagramChecker"
 import Fibonacci from "./challenges/Fibonacci"
 import TodoList from "./challenges/TodoList"
+import Capitilize from "./challenges/Capitilize"
 const App = () => {
  return (
   <div className="app">
    {/* <PalindromeChecker /> */}
    {/* <PrimeChecker /> */}
    {/* <AnagramChecker /> */}
-   {<TodoList />}
+   {/* {<TodoList />} */}
    {/* <Fibonacci /> */}
+   <Capitilize />
   </div>
  )
 }
---
+++ src/challenges/Capitilize.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+
+const Capitilize = () => {
+ const [word, setword] = useState("")
+ const [result, setResult] = useState("")
+ const capitalizeFirstLetter = (string) => {
+  const words = string.split(" ")
+  return words.map((word) => word.charAt(0).toUpperCase() + string.slice(1))
+  // let array = []
+  // for (const word of words) {
+  //  array.push(word[0].toUpperCase() + word.slice(1))
+  // }
+  // return array.join(" ")
+ }
+ const handleCapitalizeFirstLetter = () => {
+  if (word.trim() === "") {
+   setResult("Please enter a word")
+  } else {
+   setResult(capitalizeFirstLetter(word))
+  }
+ }
+ return (
+  <div>
+   <input type="text" value={word} onChange={(e) => setword(e.target.value)} />
+   <button onClick={() => handleCapitalizeFirstLetter()}>set word</button>
+   <h2>{result}</h2>
+  </div>
+ )
+}
+
+export default Capitilize
