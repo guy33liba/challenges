fb46d969bd9976c1420ad7c227b5f78d6281e216
Guy Liba <guy33liba@gmail.com>
2024-11-22 09:39:54 +0200

    בשאקע

--- src/challenges/task-tracker/Categories.js
+++ src/challenges/task-tracker/Categories.js
@@ -1,112 +1,136 @@
 import React, { useState } from "react"
 
 const Categories = ({ inputTask, addTask, deleteTask }) => {
- const [categories, setCategories] = useState({
-  sport: [],
-  health: [],
-  profession: [],
-  personal: [],
- })
- const [selectedCategory, setSelectedCategory] = useState("")
- const [isEditing, setIsEditing] = useState(null)
- const [editVAlue, setEditVAlue] = useState("")
+  const [categories, setCategories] = useState({
+    sport: [],
+    health: [],
+    profession: [],
+    personal: [],
+  })
+  const [selectedCategory, setSelectedCategory] = useState("")
+  const [isEditing, setIsEditing] = useState(null)
+  const [editVAlue, setEditVAlue] = useState("")
 
- const addTaskCategory = () => {
-  if (inputTask) {
-   setCategories((prev) => ({
-    ...prev,
-    [selectedCategory]: [...prev[selectedCategory], inputTask],
-   }))
+  const addTaskCategory = () => {
+    if (inputTask) {
+      setCategories((prev) => ({
+        ...prev,
+        [selectedCategory]: [...prev[selectedCategory], inputTask],
+      }))
+    }
   }
- }
- const deleteTaskFromCategory = (category, index) => {
-  setCategories((prev) => ({
-   ...prev,
-   [category]: prev[category].filter((_, i) => i !== index),
-  }))
- }
- const startEditing = (category, index, task) => {
-  setIsEditing({ category, index })
-  setEditVAlue(task)
- }
- const saveEditing = () => {
-  if (isEditing) {
-   const { category, index } = isEditing
-   setCategories((prev) => ({
-    ...prev,
-    [category]: prev[category].map((task, i) => {
-     return i === index ? editVAlue : task
-    }),
-   }))
+  const deleteTaskFromCategory = (category, index) => {
+    setCategories((prev) => ({
+      ...prev,
+      [category]: prev[category].filter((_, i) => i !== index),
+    }))
   }
-  setIsEditing(null)
- }
- const cancleEdit = () => {
-  setIsEditing(null)
-  setEditVAlue("")
- }
+  const startEditing = (category, index, task) => {
+    setIsEditing({ category, index })
+    setEditVAlue(task)
+  }
+  const saveEditing = () => {
+    if (isEditing) {
+      const { category, index } = isEditing
+      setCategories((prev) => ({
+        ...prev,
+        [category]: prev[category].map((task, i) => {
+          return i === index ? editVAlue : task
+        }),
+      }))
+    }
+    setIsEditing(null)
+  }
+  const cancleEdit = () => {
+    setIsEditing(null)
+    setEditVAlue("")
+  }
 
- return (
-  <div>
-   <select
-    style={{ marginLeft: "10px", height: "44px", marginTop: "10px", fontSize: "20px" }}
-    value={selectedCategory}
-    onChange={(e) => setSelectedCategory(e.target.value)}
-   >
-    <option value="">Select Category</option>
-    {Object.keys(categories).map((category) => (
-     <option ley={category} value={category}>
-      {category}
-     </option>
-    ))}
-   </select>
-   <button
-    onClick={() => {
-     addTaskCategory()
-     addTask()
-    }}
-    style={{ marginLeft: "10px", padding: "10px 20px", fontSize: "18px" }}
-   >
-    Add Task
-   </button>
-   <div className="categoryList" style={{ marginTop: "20px" }}>
-    {Object.keys(categories).map((category) => (
-     <div style={{ border: "1px solid black" }} key={category}>
-      <h3>{category}</h3>
-      <ul>
-       {categories[category].map((task, index) => (
-        <div key={index}>
-         {isEditing?.category === category && isEditing.index === index ? (
-          <div>
-           <input
-            type="text"
-            value={editVAlue}
-            onChange={(e) => setEditVAlue(e.target.value)}
-           />
-           <button onClick={saveEditing}>save</button>
-           <button onClick={cancleEdit}>cancel</button>
+  return (
+    <div>
+      <div style={{ marginLeft: "10px", height: "44px", marginTop: "10px", fontSize: "20px" }}>
+        <select
+          style={{
+            marginLeft: "10px",
+            height: "44px",
+            marginTop: "10px",
+            fontSize: "20px",
+            position: "relative",
+            left: "600px",
+          }}
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}>
+          <option value="">Select Category</option>
+          {Object.keys(categories).map((category) => (
+            <option ley={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={() => {
+            addTaskCategory()
+            addTask()
+          }}
+          style={{
+            marginLeft: "10px",
+            padding: "10px 20px",
+            fontSize: "18px",
+            position: "relative",
+            left: "600px",height: "44px",width:'200px'
+          }}>
+          Add Task
+        </button>
+      </div>
+      <div className="categoryList" style={{ marginTop: "20px" }}>
+        {Object.keys(categories).map((category) => (
+          <div style={{ border: "1px solid black" }} key={category}>
+            <h3>{category}</h3>
+            <ul>
+              {categories[category].map((task, index) => (
+                <div key={index}>
+                  {isEditing?.category === category && isEditing.index === index ? (
+                    <div>
+                      <input
+                        type="text"
+                        value={editVAlue}
+                        onChange={(e) => setEditVAlue(e.target.value)}
+                      />
+                      <button onClick={saveEditing}>save</button>
+                      <button onClick={cancleEdit}>cancel</button>
+                    </div>
+                  ) : (
+                    <div
+                      className="deleteEditBlock"
+                      style={{ width: "250px", marginBottom: "15px" }}>
+                      <span>{task}</span>
+                      <div
+                        style={{
+                          width: "250px",
+                          display: "flex",
+                          flexDirection: "column",
+                          width: "100%",
+                        }}>
+                        <button
+                          style={{ borderRadius: "10px" }}
+                          onClick={() => deleteTaskFromCategory(category, index)}>
+                          Delete
+                        </button>
+                        <button
+                          style={{ borderRadius: "10px", marginTop: "10px" }}
+                          onClick={() => startEditing(category, index, task)}>
+                          Edit
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </ul>
           </div>
-         ) : (
-          <div className="deleteEditBlock" style={{ width: "250px", background: "red" }}>
-           <li>{task}</li>
-           <div style={{ width: "250px", display: "flex", justifyContent: "end" }}>
-            <button
-             style={{ marginRight: "10px" }}
-             onClick={() => deleteTaskFromCategory(category, index)}
-            >
-             Delete
-            </button>
-            <button onClick={() => startEditing(category, index, task)}>Edit</button>
-           </div>
-          </div>
-         )}
-        </div>
-       ))}
-      </ul>
-     </div>
-    ))}
-   </div>
-  </div>
- )
+        ))}
+      </div>
+    </div>
+  )
 }
 export default Categories
--- src/challenges/task-tracker/TaskTracker.js
+++ src/challenges/task-tracker/TaskTracker.js
@@ -2,55 +2,55 @@ import React, { useState } from "react"
 import Categories from "./Categories"
 
 const TaskTracker = ({ categories }) => {
- const [inputTask, setinputTask] = useState("")
- const [tasks, setTasks] = useState([])
- const [newInputTask, setNewInputTask] = useState("")
- const addTask = (task) => {
-  if (inputTask.trim() === "") {
-   alert("Please enter a task")
-   return
+  const [inputTask, setinputTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [newInputTask, setNewInputTask] = useState("")
+  const addTask = (task) => {
+    if (inputTask.trim() === "") {
+      alert("Please enter a task")
+      return
+    }
+    setTasks([
+      ...tasks,
+      { id: tasks.length, task: inputTask, completed: false, isEditing: false, categories },
+    ])
+    setinputTask("")
   }
-  setTasks([
-   ...tasks,
-   { id: tasks.length, task: inputTask, completed: false, isEditing: false, categories },
-  ])
-  setinputTask("")
- }
- const onchangeValue = (e) => {
-  setinputTask(e.target.value)
- }
- const deleteTask = (id) => {
-  setTasks(() => tasks.filter((task) => task.id !== id))
- }
- const completeTask = (id) => {
-  setTasks((task) =>
-   task.map((task) => (task.id === id ? { ...task, completed: !task.completed } : task))
-  )
- }
- const edittasktoggle = (id) => {
-  setTasks((task) =>
-   task.map((item) => (item.id === id ? { ...item, isEditable: !item.isEditable } : item))
-  )
- }
- const editTask = (id, newTask) => {
-  setTasks((task) =>
-   task.map((task) =>
-    task.id === id ? { ...task, task: newTask, isEditable: false } : task
-   )
-  )
- }
- return (
-  <div>
-   <input
-    type="text"
-    style={{ fontSize: "30px" }}
-    value={inputTask}
-    onChange={onchangeValue}
-   />
-   <div style={{ display: "flex" }}>
-    <Categories inputTask={inputTask} addTask={addTask} deleteTask={deleteTask} />
-   </div>
-   {tasks.map((task, index) => {
+  const onchangeValue = (e) => {
+    setinputTask(e.target.value)
+  }
+  const deleteTask = (id) => {
+    setTasks(() => tasks.filter((task) => task.id !== id))
+  }
+  const completeTask = (id) => {
+    setTasks((task) =>
+      task.map((task) => (task.id === id ? { ...task, completed: !task.completed } : task)),
+    )
+  }
+  const edittasktoggle = (id) => {
+    setTasks((task) =>
+      task.map((item) => (item.id === id ? { ...item, isEditable: !item.isEditable } : item)),
+    )
+  }
+  const editTask = (id, newTask) => {
+    setTasks((task) =>
+      task.map((task) => (task.id === id ? { ...task, task: newTask, isEditable: false } : task)),
+    )
+  }
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          style={{ fontSize: "30px", marginLeft: "120px", position: "relative", left: "500px" }}
+          value={inputTask}
+          onChange={onchangeValue}
+        />
+        <div style={{ display: "flex", position: "relative" }}>
+          <Categories inputTask={inputTask} addTask={addTask} deleteTask={deleteTask} />
+        </div>
+      </div>
+      {/* {tasks.map((task, index) => {
     return (
      <div key={index}>
       {!task.isEditable ? (
@@ -87,9 +87,9 @@ const TaskTracker = ({ categories }) => {
       )}
      </div>
     )
-   })}
-  </div>
- )
+   })} */}
+    </div>
+  )
 }
 
 export default TaskTracker
