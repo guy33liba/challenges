9286205a273937ca1305944cfc3b93492f970114
Guy Liba <guy33liba@gmail.com>
2024-11-22 11:39:40 +0200

    missing

--- src/challenges/MissingNumber.js
+++ src/challenges/MissingNumber.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react"
 
 const MissingNumber = () => {
-  const array = [1, 2, 3, 4, 5, 6, 8, 9, 10]
+  const array = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12]
   const [missingNumber, setmissingNumber] = useState(null)
   function findmissingnumber() {
-    const totalSum = (10 * (10 + 1)) / 2
+    const min = Math.min(...array)
+    const max = Math.max(...array)
+
+    const totalSum = (max * (max + 1) / 2) - ((min - 1) * min) / 2
     const arraySum = array.reduce((accmulator, currentValue) => accmulator + currentValue, 0)
-    console.log(totalSum,arraySum)
+    console.log (totalSum, arraySum)
     const missing = totalSum - arraySum
     setmissingNumber(missing)
   }
