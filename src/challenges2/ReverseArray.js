2ad4f9764470d59cd81dac4dbb79b677c9424f38
guy33liba <guy33liba@gmail.com>
2024-11-24 08:11:17 +0200

    app

--- src/challenges2/ReverseArray.js
+++ src/challenges2/ReverseArray.js
@@ -2,18 +2,41 @@ import React, { useState } from "react"
 
 const ReverseArray = () => {
  const array = ["12312", "1232134", "sdfasdf", "asdfasd", "asdfasdczx"]
- const [reversingArray, setReversingArray] = useState([])
+ const [reversingArray, setReversingArray] = useState(array)
+ const [sumArray, setSumArray] = useState(0)
+ ///////////////////////////////
+
+ function toggleRevereseArray() {
+  setReversingArray(array)
+ }
+
+ ///////////////////////////////
+
  function revereseArray(str) {
-  let array = str
-  for (let i = array.length - 1; i < array.length; i--) {
-   setReversingArray(array)
+  let revereseArray = []
+  for (let i = str.length - 1; i >= 0; i--) {
+   revereseArray.push(str[i])
+  }
+  setReversingArray(revereseArray)
+
+  ///////////////////////////////
+  // const reverese = [...reversingArray].reverse()
+  // setReversingArray(reverese)
+ }
+ ///////////////////////////////
+ function sumArrayfunc(arr) {
+  for (let i = 0; i <= arr.length; i++) {
+   setSumArray((prev) => prev + i)
   }
  }
  return (
   <div>
    <button onClick={() => revereseArray(array)}>Reverse</button>
-   {array.map((item) => (
-    <div key={item}></div>
+   <button onClick={() => toggleRevereseArray()}>toggleReverese</button>
+   <button onClick={() => sumArrayfunc([1, 2, 3, 4,5 ])}>summing</button>
+   <h2>Sum of Array Elements: {sumArray}</h2>
+   {reversingArray.map((item) => (
+    <div key={item}>{item}</div>
    ))}
   </div>
  )
