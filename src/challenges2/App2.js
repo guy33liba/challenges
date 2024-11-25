0cc606a654dd1382ecd8c719700772b2b2946489
Guy Liba <guy33liba@gmail.com>
2024-11-25 14:35:43 +0200

    astix

--- src/challenges2/App2.js
+++ src/challenges2/App2.js
@@ -6,18 +6,20 @@ import MaxChar from "./MaxChar"
 import Touppercase from "./Touppercase"
 import Anagrams from "./Anagrams"
 import Todolist from "./todolist/Todolist"
+import AstrixPyramid from "./AstrixPyramid"
 const App2 = () => {
- return (
-  <div>
-   {/* <ReverseDynamicly /> */}
-   {/* <Palindrom2 /> */}
-   {/* <ReverseArray /> */}
-   {/* <MaxChar /> */}
-   {/* <Touppercase /> */}
-   {/* <Anagrams /> */}
-   <Todolist />
-  </div>
- )
+  return (
+    <div>
+      {/* <ReverseDynamicly /> */}
+      {/* <Palindrom2 /> */}
+      {/* <ReverseArray /> */}
+      {/* <MaxChar /> */}
+      {/* <Touppercase /> */}
+      {/* <Anagrams /> */}
+      {/* <Todolist /> */}
+      <AstrixPyramid />
+    </div>
+  )
 }
 
 export default App2
---
+++ src/challenges2/AstrixPyramid.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+
+const AstrixPyramid = () => {
+  const [rows, setRows] = useState(0)
+  const [pyramid, setPyramid] = useState([])
+
+  const generatePyramid = () => {
+    const tempPyramid = []
+    for (let i = 1; i <= rows; i++) {
+      const spaces = " ".repeat(rows - i)
+      const stars = "*".repeat(2 * i - 1)
+      tempPyramid.push(spaces + stars)
+      // tempPyramid.push("*".repeat(i))
+    }
+    setPyramid(tempPyramid)
+  }
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>Asterisk Pyramid</h2>
+      <input
+        type="number"
+        placeholder="Enter number of rows"
+        value={rows}
+        onChange={(e) => setRows(parseInt(e.target.value) || 0)}
+      />
+      <button onClick={generatePyramid} style={{ marginLeft: "10px" }}>
+        Generate Pyramid
+      </button>
+      <div style={{ marginTop: "20px" }}>
+        {pyramid.map((row, index) => (
+          <div key={index}>{row}</div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default AstrixPyramid
