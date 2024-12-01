9d0903f48863c04aa09b8f7ac34416c97c9fa316
Guy Liba <guy33liba@gmail.com>
2024-12-01 14:05:42 +0200

    rafc

--- src/challenges/Debounce2.js
+++ src/challenges/Debounce2.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react"
 
-const Debounce2 = (text, delay) => {
- const [debounce, setDebounce] = useState(text)
-
- useEffect(() => {
-  const timer =  setTimeout(() => {
-    setDebounce(text)
-   }, delay)}
-   return clearTimeout(timer)
- }, [debounce])
-
- return (
-  <div>
-   <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-   <button onClick={() => timer(input, 500)}>Debounce</button>
-   <h2>{debounce}</h2>
-  </div>
- )
+export const useDebounce = (value, delay) => {
+  const [debounce, setDebounce] = useState("")
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounce(value)
+    }, delay)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+  return debounce
+}
+const Debounce2 = () => {
+  const [input, setinput] = useState("")
+  const debouncedInput = useDebounce(input,1300)
+  return (
+    <div>
+      <input type="text" value={input} onChange={(e) => setinput(e.target.value)} />
+      <p>Debounced input: {debouncedInput}</p>
+    </div>
+  )
 }
 
 export default Debounce2
