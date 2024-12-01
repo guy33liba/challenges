36f08f573b2b1020cab3c48f5a351ec5c259214a
guy33liba <guy33liba@gmail.com>
2024-12-01 20:37:10 +0200

    css

--- src/challenges2/memory-game/MemoryGame2.js
+++ src/challenges2/memory-game/MemoryGame2.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react"
 import "./MemoryGame2.css"
 const MemoryGame2 = () => {
-  const [cardsArray, setCardsArray] = useState([])
-  let cards = ["card 1", "card 2", "card 3", "card 4", "card 5", "card 6"]
-  function handleCards(items) {
-    let newCardsArray = []
-    for (let i = 0; i < items.length; i++) {
-      let element = items[i]
-      newCardsArray.push(element, element)
-    }
-    setCardsArray(newCardsArray)
+ const [cardsArray, setCardsArray] = useState([])
+ let cards = ["card 1", "card 2", "card 3", "card 4", "card 5", "card 6"]
+ function handleCards(items) {
+  let newCardsArray = []
+  for (let i = 0; i < items.length; i++) {
+   let element = items[i]
+   newCardsArray.push(element, element)
   }
-  console.log(cardsArray)
-  return (
-    <div>
-      <button onClick={() => handleCards(cards)}>cards</button>
-      <div className="cardsGrid">
-        {
-          <div className="cardItem">
-            {cardsArray.map((item) => {
-              return <div key={item}>{item}</div>
-            })}
-          </div>
-        }
+  setCardsArray(newCardsArray)
+ }
+ console.log(cardsArray)
+ return (
+  <div>
+   <button onClick={() => handleCards(cards)}>cards</button>
+   <div className="cardsGrid">
+    {cardsArray.map((item) => {
+     return (
+      <div className="cardItem" key={item}>
+       <button>{item}</button>
       </div>
-    </div>
-  )
+     )
+    })}
+   </div>
+  </div>
+ )
 }
 
 export default MemoryGame2
