73d7da2ae09dea6a87297452b200c1a87ae755fa
Guy Liba <guy33liba@gmail.com>
2024-12-02 12:31:44 +0200

    asa

--- src/challenges2/memory-game/MemoryGame2.js
+++ src/challenges2/memory-game/MemoryGame2.js
@@ -1,31 +1,52 @@
 import React, { useState } from "react"
 import "./MemoryGame2.css"
+
 const MemoryGame2 = () => {
- const [cardsArray, setCardsArray] = useState([])
- let cards = ["card 1", "card 2", "card 3", "card 4", "card 5", "card 6"]
- function handleCards(items) {
-  let newCardsArray = []
-  for (let i = 0; i < items.length; i++) {
-   let element = items[i]
-   newCardsArray.push(element, element)
+  const [cardsArray, setCardsArray] = useState([])
+  const [flippedStates, setflippedStates] = useState([])
+  let cards = [
+    "photos/photo1.png",
+    "photos/photo2.png",
+    "photos/photo3.png",
+    "photos/photo4.png",
+    "photos/photo5.png",
+    "photos/photo6.png",
+  ]
+  function handleCards(items) {
+    let newCardsArray = []
+    for (let i = 0; i < items.length; i++) {
+      let element = items[i]
+      newCardsArray.push(element, element)
+    }
+    setCardsArray(newCardsArray)
   }
-  setCardsArray(newCardsArray)
- }
- console.log(cardsArray)
- return (
-  <div>
-   <button onClick={() => handleCards(cards)}>cards</button>
-   <div className="cardsGrid">
-    {cardsArray.map((item) => {
-     return (
-      <div className="cardItem" key={item}>
-       <button>{item}</button>
+  const handleFlip = (index) => {
+    const updatedFlippedStates = [...flippedStates]
+    updatedFlippedStates[index] = !updatedFlippedStates[index]
+    setflippedStates(updatedFlippedStates)
+  }
+  return (
+    <div>
+      <button style={{ marginLeft: "20px" }} onClick={() => handleCards(cards)}>
+        Start Game
+      </button>
+      <div className="cardsGrid">
+        {cardsArray.map((item, index) => {
+          return (
+            <div className="cardItem" key={index}>
+              <button
+                className={`cardButton ${flippedStates[index] ? "cardFlipped" : "cardFlipped2"}`}
+                onClick={() => handleFlip(index)}>
+                <div className="cardContent">
+                  {<img src={flippedStates[index] ? item : "photos/photo7.png"} alt="Hidden" />}
+                </div>
+              </button>
+            </div>
+          )
+        })}
       </div>
-     )
-    })}
-   </div>
-  </div>
- )
+    </div>
+  )
 }
 
 export default MemoryGame2
