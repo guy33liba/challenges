40adc100e124c46ea14e23d28bb23b597417d333
Guy Liba <guy33liba@gmail.com>
2025-01-20 17:45:44 +0200

    asdfa

--- src/App.js
+++ src/App.js
@@ -1,66 +1,29 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import { useState } from "react"
 
 const App = () => {
-  const cards = [
-    {
-      id: 1,
-      symbol: "A",
-      flipped: false,
-      mathched: false,
-    },
-    {
-      id: 2,
-      symbol: "B",
-      flipped: false,
-      mathched: false,
-    },
-    {
-      id: 3,
-      symbol: "C",
-      flipped: false,
-      mathched: false,
-    },
-    {
-      id: 4,
-      symbol: "D",
-      flipped: false,
-      mathched: false,
-    },
+  const cardsArray = [
+    { id: 1, symbol: "A", flipped: false, ismatched: false },
+    { id: 1, symbol: "B", flipped: false, ismatched: false },
+    { id: 1, symbol: "C", flipped: false, ismatched: false },
+    { id: 1, symbol: "A", flipped: false, ismatched: false },
+    { id: 1, symbol: "B", flipped: false, ismatched: false },
+    { id: 1, symbol: "C", flipped: false, ismatched: false },
   ]
-  const [cardGame, setCardGame] = useState([...cards, ...cards])
-  const [flippedCards, setFlippedCards] = useState([])
-  const [score, setscore] = useState(0)
-  const [mathched, setMathched] = useState([])
-  useEffect(() => {
-    if (flippedCards.length < 2) {
-      const [firstCard, secondcard] = flippedCards
-      if (firstCard.symbol === secondcard.symbol) {
-        setMathched((prevmatched) => [...prevmatched, firstCard.id, secondcard.id])
-        setscore((prev) => prev + 1)
-      }
-      setTimeout(() => {
-        setFlippedCards([])
-      }, 1000)
-    }
-  })
+  const shuffleCards = [...cardsArray].sort(() => Math.random() - 0.5)
+
+  const [cardlist, setCardlist] = useState(randomCards)
+  const flipcard = (id) => {
+    cardlist.map((card) => (card.id === id ? { ...card, flipped: !card.flipped } : card))
+  }
   return (
-    <div>
-      <h1>Memory Game</h1>
-      <div style={{ margin: "auto 0", display: "flex", justifyContent: "center", width: "120px", flexWrap: "wrap", textAlign: "center" }}>
-        {cardGame.map((card, index) => (
-          <div>
-            {index}
-            <button
-              onClick={() => handleFlipppedCArd(card)}
-              style={{
-                width: "50px",
-                fontSize: "30px",
-                textAlign: "center",
-                backgroundColor: flippedCards.includes(card) || card.symbol ? "#ddd" : "#fff",
-              }}>
-              {flippedCards.includes(card) || card.mathched ? card.symbol : "?"}
-            </button>
-          </div>
+    <div style={{ textAlign: "center" }}>
+      <h2>card game</h2>
+      <div style={{ display: "flex", width: "130px", flexWrap: "wrap", justifyContent: "center", margin: "0 auto" }}>
+        {cardsArray.map((item) => (
+          <button onClick={() => flipcard(item.id)} style={{ fontSize: "40px" }}>
+            {item.flipped ? item.symbol : "?"}
+          </button>
         ))}
       </div>
     </div>
