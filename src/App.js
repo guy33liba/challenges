db601028bb4dbb49464559610a781290a66a0fb7
Guy Liba <guy33liba@gmail.com>
2025-01-21 14:21:57 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,82 +1,8 @@
-import React, { useState } from "react"
-import { useEffect } from "react"
+import React from 'react'
 
 const App = () => {
-  const questions = [
-    {
-      question: "What is the capital of France?",
-      answers: ["Paris", "Rome", "Berlin", "Madrid"],
-      correctAnswer: "Paris",
-    },
-    {
-      question: "Which planet is known as the Red Planet?",
-      answers: ["Earth", "Mars", "Jupiter", "Venus"],
-      correctAnswer: "Mars",
-    },
-    {
-      question: "Who wrote 'Romeo and Juliet'?",
-      answers: ["William Shakespeare", "Mark Twain", "Charles Dickens", "Jane Austen"],
-      correctAnswer: "William Shakespeare",
-    },
-  ]
-  const [questionIndex, setQuestionIndex] = useState(0)
-  const [correct, setCorrect] = useState(false)
-  const [score, setscore] = useState(0)
-  const [message, setMessage] = useState("hello")
-  if (questionIndex > 2) {
-    setQuestionIndex(0)
-  }
-  function setnextquestion() {
-    if (questionIndex < 2) {
-      setQuestionIndex((prev) => prev + 1)
-      setCorrect(false)
-    } else {
-      setQuestionIndex(0)
-      setCorrect(false)
-    }
-  }
-  const currentQuestion = questions[questionIndex]
-
-  function handleAnswer(answer) {
-    const currentQuestion = questions[questionIndex]
-    if (answer === currentQuestion.correctAnswer) {
-      setMessage(`Correct Answer: ${currentQuestion.correctAnswer}`)
-      setCorrect(true)
-      setscore((prev) => prev + 1)
-    } else {
-      console.log(`Incorrect! The correct answer is: ${currentQuestion.correctAnswer}`)
-      setCorrect(false)
-    }
-  }
-
   return (
-    <div style={{ marginLeft: "320px" }}>
-      <h2>Quiz Game</h2>
-      <div>{questions[questionIndex].question}</div>
-      <ul>
-        {questions[questionIndex].answers.map((item, index) => (
-          <ul style={{ width: "200px" }} key={index}>
-            <li>
-              <button
-                onClick={() => handleAnswer(item)}
-                style={{
-                  backgroundColor: correct && item === currentQuestion.correctAnswer ? "green" : "",
-                  marginTop: "10px",
-                  height: "30px",
-                  width: "200px",
-                }}>
-                {item}
-              </button>
-            </li>
-          </ul>
-        ))}
-      </ul>
-      <button style={{ marginLeft: "120px" }} onClick={() => setnextquestion()}>
-        Next Question
-      </button>
-      <h2>{score}</h2>
-      <h2>{message}</h2>
-    </div>
+    <div>App</div>
   )
 }
 
